5c2a657ac89b036856f25486575e2f7819f247dd
nikolaiwarner <n@nikolaiwarner.com>
2011-02-09 20:26:49 -0500

    it now determines if its been too long, awww

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,4 +2,18 @@ class Friend < ActiveRecord::Base
   belongs_to :frequency
   belongs_to :user
   has_many :interactions
+  
+  def needs_an_interaction? user
+    interaction = Interaction.where(:user_id => user.id).order(:datetime).last
+    return days_since_interaction(interaction, Time.now) > self.frequency.number_of_days 
+  end
+  
+  def seconds_since_interaction interaction, datetime=Time.now
+    datetime.to_time.to_i - interaction.datetime.to_time.to_i
+  end
+  
+  def days_since_interaction interaction, datetime=Time.now
+    seconds_since_interaction(interaction, datetime) / 86400
+  end
+  
 end
