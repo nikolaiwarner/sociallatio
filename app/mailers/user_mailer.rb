8e74eed304c3e1b788dacbd602382066bc3615d3
nikolaiwarner <n@nikolaiwarner.com>
2011-02-23 00:13:18 -0500

    minor mailer tweeeeks

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default :from => "today@sociallatio.com"
+  default :from => "friends@sociallatio.com"
  
   def status_email(user)
     @user = user
     @url  = "http://sociallatio.com"
-    mail(:to => user.email, :subject => "[sociallatio] status update")
+    mail(:to => user.email, :subject => "[sociallatio] your status today")
   end
   
 end
